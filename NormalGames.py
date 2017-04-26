477ecf97c263e76b027b5c23b41f09e6371781b7
Dmitrii Rykunov <dprykunov@gmail.com>
2017-04-26 22:27:57 +0300

    new normal-form games added

--- NormalGames.py
+++ NormalGames.py
@@ -75,6 +75,170 @@ class NormalFormGame(Game):
 
         return payoffs
 
+    def calculate_expected_payoffs(self, pl1_strategy, pl2_strategy):
+        payoff = []
+        pl1_strategy = np.array(pl1_strategy).reshape(1, -1)
+        pl2_strategy = np.array(pl2_strategy).reshape(1, -1)
+        pl1_payoff = reduce(np.dot, [pl1_strategy, self.payoffs_matrix[0], pl2_strategy.T])
+        pl1_payoff.shape = (1,)
+        pl2_payoff = reduce(np.dot, [pl1_strategy, self.payoffs_matrix[1], pl2_strategy.T])
+        pl2_payoff.shape = (1,)
+        return pl1_payoff, pl2_payoff
+
+class PrisonersDilemma(NormalFormGame):
+    """Basic PrisonersDilemma game 2x2"""
+    players_count = 2
+    payoffs_matrix = np.array(
+        [[[-1, -3],
+          [0, -2]],
+
+         [[-1, 0],
+          [-3, -2]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Cooperate": 0, "Defect": 1}
+    allowed_moves["Player_2"] = {"Cooperate": 0, "Defect": 1}
+
+    # payoffs_matrix.flags.writeable = False
+
+    def __init__(self):
+        pass
+
+
+class StagHunt(NormalFormGame):
+    payoffs_matrix = np.array(
+    [[[10, 0],
+      [8, 7]],
+
+     [[10, 8],
+      [0, 7]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Stag": 0, "Hunt": 1}
+    allowed_moves["Player_2"] = {"Stag": 0, "Hunt": 1}
+
+    def __init__(self):
+        pass
+
+class OnlyMixed2x2(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+    [[[4, 11],
+      [8, 10]],
+
+     [[3, 0],
+      [0, 2]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Strategy_1": 0, "Strategy_2": 1}
+    allowed_moves["Player_2"] = {"Strategy_1": 0, "Strategy_2": 1}
+
+    def __init__(self):
+        pass
+
+class OnlyOne2x2(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+    [[[4, 0],
+      [-1, 2]],
+
+     [[1, 0],
+      [0, 2]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Strategy_1": 0, "Strategy_2": 1}
+    allowed_moves["Player_2"] = {"Strategy_1": 0, "Strategy_2": 1}
+
+    def __init__(self):
+        pass
+
+
+class ChoosingSides(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+    [[[10, 0],
+      [0, 10]],
+
+     [[10, 0],
+      [0, 10]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Left": 0, "Right": 1}
+    allowed_moves["Player_2"] = {"Left": 0, "Right": 1}
+
+    def __init__(self):
+        pass
+
+
+class BoS(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+        [[[4, 0, 0],
+          [0, 2, 1]],
+
+         [[2, 0, 1],
+          [0, 4, 3]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"B": 0, "S": 1}
+    allowed_moves["Player_2"] = {"B": 0, "S": 1, "X": 0}
+
+    def __init__(self):
+        pass
+
+
+class RockPaperScissors(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+        [[[0, -1, 1],
+          [1, 0, -1],
+          [-1, 1, 0]],
+
+         [[0, 1, -1],
+          [-1, 0, 1],
+          [1, -1, 0]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"R": 0, "P": 1, "S": 2}
+    allowed_moves["Player_2"] = {"R": 0, "P": 1, "S": 2}
+
+    def __init__(self):
+        pass
+
+
+class Chicken(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+    [[[0, 7],
+      [2, 6]],
+
+     [[0, 2],
+      [7, 6]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Dare": 0, "Chicken": 1}
+    allowed_moves["Player_2"] = {"Dare": 0, "Chicken": 1}
+
+
+    def __init__(self):
+        pass
+
+class SwerveStay(NormalFormGame):
+    players_count = 2
+    payoffs_matrix = np.array(
+    [[[0, -1],
+      [1, -20]],
+
+     [[0, 1],
+      [-1, -20]]])
+    players = ["Player_1", "Player_2"]
+    allowed_moves = {}
+    allowed_moves["Player_1"] = {"Swerve": 0, "Stay": 1}
+    allowed_moves["Player_2"] = {"Swerve": 0, "Stay": 1}
+
+
+    def __init__(self):
+        pass
 
 class DecisionSet(object):
     def __init__(self):
