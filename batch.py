597e4df8d865b82e2a083949d5fba1ab50b6929f
Dmitrii Rykunov <dprykunov@gmail.com>
2017-05-01 13:06:11 +0300

    update batch script -> make module-like

--- batch.py
+++ batch.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 import evgt as eg
 from concurrent import futures
 import time
+import copy
 
 
 def make_kwargs_mesh(params_to_mesh, strict_params={}):
@@ -45,16 +46,8 @@ def evaluate_model(kwargs):
     model.optimize()
 
 
-def main():
+def batch_eval(variable_params, constant_params, sample_size):
     print("Script started")
-
-    # Setup parameters
-    sample_size = 2
-    variable_params = {'popsize': (4, 400, 3), 'npairs': (1, 500, 1),
-                       'ngames': (1, 200, 2), 'dropout_rate': (1, 50, 10),
-                       'mutation_magnitude': (1, 40, 1)}
-    constant_params = {'generations': 1, 'model': eg.Chicken(),
-                       'outfolder': "test_testlog"}
 
     print("Variable params:", variable_params)
     print("Constant params:", constant_params)
@@ -83,17 +76,31 @@ def main():
 
     print("MODELS generating MC experiment data calculation")
     print("Current timestamp: {}".format(arch_start))
-    print("Current date and time: {}".format(time.strftime("%d %b %Y %H:%M:%S", time.localtime())))
+    print("Current date and time: {}".format(
+        time.strftime("%d %b %Y %H:%M:%S", time.localtime())))
     print("Total number of func evals to do: {}".format(total_func_evals))
+    print("Total number of model runs: {}".format(
+        len(kwargs_list) * sample_size))
     # print("Worst-case estimation of time to compute: {}".format(
     #     time.strftime("%H:%M:%S", time.gmtime(est_total_time))))
-    print("Worst-case estimation of time to compute: {}:{}:{}".format(s//3600, s%3600//60, s%60))
+    print("Worst-case estimation of time to compute: {}:{}:{}".format(
+        int(s // 3600), int(s % 3600 // 60), int(s % 60)))
 
+    # Create concurent async execution handler
     pool = futures.ProcessPoolExecutor(max_workers=16)
     fs = []
+    # Unpack kwargs_list
     for kwargs in kwargs_list:
+        # Hndle sample_size > 1
         for i in range(sample_size):
-            fs.append(pool.submit(evaluate_model, kwargs))
+            # Create a copy of kwargs and change outfile_prefix to distinguish samples
+            tmp_kwargs = copy.copy(kwargs)
+            tmp_kwargs["outfile_prefix"] = kwargs["outfile_prefix"] + '-' + str(i)
+
+            # Assign jobs to the async executor
+            fs.append(pool.submit(evaluate_model, tmp_kwargs))
+
+    print("Jobs assigned")
 
     for ft in futures.as_completed(fs):
         pb.update(1)
@@ -108,6 +115,19 @@ def main():
     print("Script finished successfully in {} seconds".format(
         int(arch_stop - arch_start)))
 
+
+def main():
+    # Setup parameters
+    sample_size = 1
+    variable_params = {'popsize': (4, 80, 2), 'npairs': (1, 80, 2),
+                       'ngames': (1, 30, 2), 'dropout_rate': (1, 50, 2),
+                       'mutation_magnitude': (1, 40, 2)}
+    constant_params = {'generations': 20, 'model': eg.Chicken(),
+                       'outfolder': "testsample",
+                       'outfile_prefix': 'ee'}
+    batch_eval(variable_params=variable_params,
+               constant_params=constant_params,
+               sample_size=sample_size)
 
 if __name__ == '__main__':
     main()
